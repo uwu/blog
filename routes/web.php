a12695e7d2191d41298ac5f6351f12f7bf4eff0c
Alyxia Sother <alyxia@riseup.net>
2022-07-01 11:35:26 +0000

    [AppServiceProvider] Fix application root URL

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', 'HomeController@index');
+// Route::get('/', 'HomeController@index');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
