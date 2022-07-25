d97e16ee97feba2520944d023b5106e5ac46459b
Alyxia Sother <alyxia@riseup.net>
2022-07-25 09:58:24 +0000

    [views/components] Move to tailwind

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,16 +1,16 @@
 @props(['title', 'author'])
 
-<div class="card mx-2">
+<div class="overflow-hidden bg-white dark:bg-slate-600 dark:text-white rounded-lg relative cursor-pointer mx-2">
     <a {{ $attributes }}>
-        <div class="card-body">
-            <h2 class="card-title">{{ $title }}</h2>
-            <p class="card-description">...</p>
+        <div class="m-4 mt-0">
+            <h2 class="leading-6 mt-4 mb-2 mx-auto">{{ $title }}</h2>
+            <p class="text-[#616b74] dark:text-gray-200">...</p>
 
-            <div class="card-profile">
+            <div class="flex mt-8 items-center">
                 <img src="https://cdn.discordapp.com/emojis/843188323577102367.webp?size=96&quality=lossless"
-                    alt="" class="profile-img">
-                <div class="card-profile-info">
-                    <h3 class="profile-name">{{ $author }}</h3>
+                    alt="" class="w-10 h-10 object-cover rounded-lg">
+                <div class="ml-2">
+                    <h3 class="text-base">{{ $author }}</h3>
                 </div>
             </div>
         </div>
